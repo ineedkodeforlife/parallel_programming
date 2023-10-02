f3db235cab9a5f167f03a7ef28c3311e475821a9
ineedkodeforlife <109513419+ineedkodeforlife@users.noreply.github.com>
2023-10-02 21:03:21 +0300

    add lab_2

---
+++ par_prog_2.cpp
@@ -0,0 +1,37 @@
+﻿#include <omp.h>
+#include <iostream>
+#include<vector>
+using namespace std;
+
+
+const int NMAX = 500;
+const int LIMIT = 500;
+void main()
+{
+    double start, end;
+    int i, j;
+    float sum;
+   //vector<std::vector<int>> a(NMAX, std::vector<int>(NMAX));
+    float a[NMAX][NMAX];
+
+    for (i = 0; i < NMAX; i++)
+        for (j = 0; j < NMAX; j++)
+            a[i][j] = i+j;
+
+
+start = omp_get_wtime();
+#pragma omp parallel shared(a) if (NMAX>LIMIT)
+    {
+#pragma omp for private(i,j,sum) 
+        for (i = 0; i < NMAX; i++)
+        {
+            sum = 0;
+            for (j = 0; j < NMAX; j++)
+                sum += a[i][j];
+            //cout << "sum elements string: " << i << "  " << sum << endl;
+        }
+    } /* Завершение параллельного фрагмента */
+
+end = omp_get_wtime();
+cout << "Time work = " << end - start;
+}
